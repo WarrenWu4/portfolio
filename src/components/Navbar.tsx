f4802a0e98f616c43802cb5e895a177861acefd2
WarrenWu4 <warrenweiwu04@gmail.com>
2023-05-28 01:33:27 -0500

    idfk what im doing anywhere, it's late at night... too tired

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,32 +1,57 @@
-import { useState } from "react";
 import "./Navbar.css";
+import { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 
 export default function Navbar() {
 
-    const [showSide, SetShowSide] = useState("none");
-    
-    const handleSide = () => {
-        if (showSide === "none") {
-            SetShowSide("flex");
+    const [isMobile, setIsMobile] = useState((window.innerWidth < 640) ? true:false);
+    const [showSide, setShowSide] = useState(false);
+
+    // const ref = useRef(null);
+    // const refBtn = useRef(null);
+
+    useEffect(() => {
+        // const checkExit = e => {
+        //     if (side === "flex" && ref.current && !ref.current.contains(e.target) && refBtn.current && !refBtn.current.contains(e.target) && window.innerWidth < 768) {
+        //         setSide("none");
+        //     }
+        // }
+        const checkSize = () => {(window.innerWidth < 640) ? setIsMobile(true):setIsMobile(false);}
+
+        window.addEventListener("resize", checkSize);
+        // document.addEventListener("mousedown", checkExit);
+
+        return () => {
+            window.removeEventListener("resize", checkSize);
+            // document.removeEventListener("mousedown", checkExit);
         }
-        else {
-            SetShowSide("none");
+    })
+
+    const sideView = () => {
+        if (!showSide) {
+            setShowSide(true);
+        } else {
+            setShowSide(false);
         }
     }
 
+
     return (
         <nav className="nav">
+
             <NavLink to="/" >
                 <img src="/logo.svg" alt="logo" className="nav-logo"/>
             </NavLink>
-            <img src="/menu.svg" alt="menu" className="nav-menu" onClick={handleSide}/>
-            <div className="nav-links" style={{display: showSide}}>
+
+            <img src={(!showSide) ? "/menu.svg":"/close.svg"} alt="menu" className="nav-menu" style={{display: (isMobile) ? "flex":"none"}} onClick={sideView}/>
+
+            <div className={(!isMobile) ? "nav-links-row":"nav-links-col"} style={{display: (isMobile && !showSide) ? "none":"flex"}}>
                 <NavLink to="/home">Home</NavLink>
                 <NavLink to="/projects">Projects</NavLink>
                 <NavLink to="/experiences">Experiences</NavLink>
                 <NavLink to="/about">About</NavLink>
             </div>
+
         </nav>
     )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,41 +5,51 @@ import Navbar from "../components/Navbar";
 
 export default function Home() {
 
+    const secret = () => {
+        alert("work in progress :D");
+    }
+
     return (
         <div className="page">
 
-            <div className="blob-wrapper">
-                <div id="blob">
-                    Hi! I'm blob. I'll be following you around. Hope you don't mind :D
-                    <span/>
-                    <span/>
-                    <span/>
+            <div className="container">
+                <Navbar/>
+
+                <div className="home-content">
+
+                    <div className="blob">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <div className="secret" onClick={secret}>Click Me</div>
+                    </div>
+
+
+                    <div className="home-group">
+                        <div className="home-header">Hi, I'm Warren</div>
+                        <div className="home-subheader">I'm a computer science student and I make software that helps people.</div>
+
+                        <div className="socials">
+                            <a href="https://github.com/WarrenWu4" target="_blank">
+                                <FaGithub/>
+                            </a>
+                            <a href="https://www.linkedin.com/in/warren-wu4/" target="_blank">
+                                <FaLinkedin/>
+                            </a>
+                            <a href="https://github.com/WarrenWu4" target="_blank">
+                                <SiGmail/>
+                            </a>
+                            <a href="https://github.com/WarrenWu4" target="_blank">
+                                <FaYoutube/>
+                            </a>
+                        </div>
+                    </div>
+
                 </div>
+
             </div>
 
-            <Navbar/>
-
-            <div className="home-content">
-                <div className="home-profile">
-                    <div id="easter-egg">Made with 💚 by Warren Wu</div>
-                </div>
-                <div className="home-header">Hi, I'm Warren</div>
-                <div className="home-subheader">A computer science student on a mission to improve the world</div>
-                <div className="socials">
-                    <a href="https://github.com/WarrenWu4" target="_blank">
-                        <FaGithub/>
-                    </a>
-                    <a href="https://www.linkedin.com/in/warren-wu4/" target="_blank">
-                        <FaLinkedin/>
-                    </a>
-                    <a href="https://github.com/WarrenWu4" target="_blank">
-                        <SiGmail/>
-                    </a>
-                    <a href="https://github.com/WarrenWu4" target="_blank">
-                        <FaYoutube/>
-                    </a>
-                </div>
-            </div>
+            <div className="easter-egg">Made with 💚 by Warren Wu</div>
 
         </div>
     )
