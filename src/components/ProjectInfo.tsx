a8b2b3142918dfcfc82fbabd9ad45d83076baa3f
WarrenWu4 <warrenweiwu04@gmail.com>
2023-06-19 13:33:06 -0500

    test2

--- src/components/ProjectInfo.tsx
+++ src/components/ProjectInfo.tsx
@@ -1,9 +1,9 @@
 import { motion } from "framer-motion";
-import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { ReactMarkdown } from "react-markdown/lib/react-markdown";
 import data from "../data/proj.json";
 import { BiLinkExternal } from "react-icons/bi";
+import { useEffect, useState } from "react";
 
 export default function ProjectInfo() {
 
@@ -12,7 +12,8 @@ export default function ProjectInfo() {
 
     const [mdInfo, setMdInfo] = useState("")
     useEffect(() => {
-        fetch('/src/data/proj_articles'+data.projects[id.id-1].links[0]).then(res=>res.text()).then(text=>setMdInfo(text))
+        fetch('/src/data/proj_articles'+data.projects[id.id-1].links[0]).then(res=>res.text()).then(text=>setMdInfo(text));
+        console.log(mdInfo)
     })
 
     const githubComp = (data.projects[id.id-1].links.length >= 2) ? 
@@ -29,15 +30,15 @@ export default function ProjectInfo() {
     :
     ""
 
+    const test = `### testing test test testing test random test`
+
     return (
         <motion.div exit={{opacity: 0}} initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{duration:0.4, ease:"easeInOut"}} className="w-screen h-screen flex flex-col items-center relative">
             <div className="mt-[12.8rem] text-[black] dark:text-dark-100 font-default font-bold text-[3.6rem]">{title}</div>
             
 
             <div className="max-w-[1536px] w-screen flex flex-col px-[1.6rem] tablet:px-[6.4rem] desktop:px-[12.8rem] [&>h2]:text-[black] [&>h2]:dark:text-dark-100 [&>h2]:font-bold [&>h2]:text-[2.4rem] [&>h2]:my-[2.4rem] [&>h3]:text-neutral-380 [&>h3]:dark:text-dark-360 [&>h3]:text-[1.6rem] [&>h3]:font-semibold font-default">
-                <ReactMarkdown>
-                    {mdInfo}
-                </ReactMarkdown>
+                <ReactMarkdown children={test} />
             </div>
 
             <div className="max-w-[1536px] w-screen flex px-[1.6rem] tablet:px-[6.4rem] desktop:px-[12.8rem] py-[4.8rem] [&>*]:mr-[2rem]">
