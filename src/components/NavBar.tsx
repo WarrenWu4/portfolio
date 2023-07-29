0736e89dedd552b70abd0ba8a31ef9cf6cfe966f
WarrenWu4 <warrenweiwu04@gmail.com>
2023-07-28 21:07:27 -0500

    added theme btn change

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,40 +1,52 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect, useMemo } from "react"
 import { NavLink } from "react-router-dom";
 import { BsHouseDoorFill, BsFillPersonFill, BsFolderFill } from "react-icons/bs";
 import { IoMdBriefcase } from "react-icons/io"
 import { useLocation } from "react-router-dom";
 
 import logo from "/img/logo.svg"
-// import theme from "/img/dark_mode.svg"
+import themeDark from "/img/dark_mode.svg"
+import themeLight from "/img/light_mode.svg"
 
 export default function Nav() {
 
-    // const changeTheme = (e:any) => {
-    //     e.preventDefault()
-    //     console.log("WORK IN PROGRESS")
-    // }
+    const [mode, setMode] = useState((localStorage.getItem("theme") === "dark") ? "dark":"light")
+    const [themeIcon, setThemeIcon] = useState((mode === "light") ? themeDark:themeLight)
+
+    const changeTheme = () => {
+        const newTheme = (mode === "light") ? "dark" : "light"
+        localStorage.setItem("theme", newTheme)
+        setMode(newTheme)
+    }
+    useMemo(() => {
+        setThemeIcon((mode === "light") ? themeDark:themeLight)
+    }, [mode])
 
     const path:string = useLocation().pathname
 
+
+
     return (
-        <nav className="max-w-[768px] mt-[3.2rem] h-[5rem] flex items-center w-full px-[1.6rem] small:px-[3.2rem] med:px-[6.4rem] [&>*]:mr-[0.8rem] small:[&>*]:mr-[1.6rem] font-bold text-[2rem] text-black/70 ">
+        <nav className="max-w-[768px] mt-[3.2rem] h-[5rem] flex justify-between items-center w-full px-[1.6rem] small:px-[3.2rem] med:px-[6.4rem] font-bold text-[1.8rem] text-black/70 ">
 
-            <NavLink to={"/"}>
-                <img src={logo} alt="logo" className="w-[5rem] aspect-square"/>
-            </NavLink>
+            <div className="flex items-center [&>*]:mr-[0.8rem] small:[&>*]:mr-[1.6rem]">
+                <NavLink to={"/"}>
+                    <img src={logo} alt="logo" className="w-[4.8rem] aspect-square"/>
+                </NavLink>
 
-            <NavLink 
-            className={(path.split("/")[1] === "proj") ? 'is-active':'not-active'} 
-            to="/proj">
-                    PROJ
-            </NavLink>
+                <NavLink 
+                className={(path.split("/")[1] === "proj") ? 'is-active':'not-active'} 
+                to="/proj">
+                        PROJ
+                </NavLink>
 
-            <NavLink className={(path.split("/")[1] === "blog") ? 'is-active':'not-active'}  to="/blog" >BLOG</NavLink>
-            <NavLink className={(path.split("/")[1] === "misc") ? 'is-active':'not-active'}  to="/misc" >MISC</NavLink>
+                <NavLink className={(path.split("/")[1] === "blog") ? 'is-active':'not-active'}  to="/blog" >BLOG</NavLink>
+                <NavLink className={(path.split("/")[1] === "misc") ? 'is-active':'not-active'}  to="/misc" >MISC</NavLink>
+            </div>
 
-            {/* <button type="button" onClick={changeTheme}>
-                <img src={theme} alt="theme" className="w-[2.4rem] aspect-square" />
-            </button> */}
+            <button type="button" onClick={changeTheme}>
+                <img src={themeIcon} alt="theme" className="w-[2.8rem] aspect-square" />
+            </button>
 
         </nav>
     )
