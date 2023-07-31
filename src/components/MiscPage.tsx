6a64500dc626caa8a3a98014c265edf2fd4302ec
WarrenWu4 <warrenweiwu04@gmail.com>
2023-07-31 13:35:35 -0500

    fixed spotify links

--- src/components/MiscPage.tsx
+++ src/components/MiscPage.tsx
@@ -84,10 +84,10 @@ export const MyMusic = () => {
     const SpotifyCard = (track:any) => {
         console.log(track)
         return (
-        <a href={track.track.href} target={"_blank"} className="w-full rounded-[1.2rem] shadow-elevate dark:shadow-elevate-dark p-[1.6rem] cursor-pointer flex items-center hover:scale-105 transition">
+        <a href={track.track.uri} target={"_blank"} className="w-full rounded-[1.2rem] shadow-elevate dark:shadow-elevate-dark p-[1.6rem] cursor-pointer flex items-center hover:scale-105 transition">
             <div className="mr-[2rem] text-[2.8rem]" >{track.number+1}</div>
             <img src={track.track.album.images[2].url} alt="album image" width={40} height={40} className="rounded-[0.8rem] mr-[1.2rem]"/>
-            <div className="flex flex-col justify-center">
+            <div className="flex flex-col justify-center mr-[2rem]">
                 <div className="mb-[0.4rem] text-[1.6rem]">{track.track.name}</div>
                 <div className="text-[1.2rem] text-black/70 dark:text-white/70">{track.track.artists[0].name}</div>
             </div>
