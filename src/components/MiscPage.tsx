0060b7c7f69727dd1fe15330466c13b1b863c769
WarrenWu4 <warrenweiwu04@gmail.com>
2023-07-31 22:11:58 -0500

    fixed hover issue

--- src/components/MiscPage.tsx
+++ src/components/MiscPage.tsx
@@ -90,12 +90,14 @@ export const MyMusic = () => {
         const timer = 1
         
         return (
-        <motion.a initial="hidden" whileInView="visible" viewport={{once: true}} transition={{duration:timer, ease:"easeInOut"}} variants={variants} href={track.track.uri} target={"_blank"} className="w-full rounded-[1.2rem] shadow-elevate dark:shadow-elevate-dark p-[1.6rem] cursor-pointer flex items-center hover:scale-105 transition">
+        <motion.a initial="hidden" whileInView="visible" viewport={{once: true}} transition={{duration:timer, ease:"easeInOut"}} variants={variants} href={track.track.uri} target={"_blank"} className="w-full">
+            <div className="w-full rounded-[1.2rem] shadow-elevate dark:shadow-elevate-dark p-[1.6rem] cursor-pointer flex items-center hover:scale-105 transition">
             <div className="mr-[2rem] text-[2.8rem]" >{track.number+1}</div>
             <img src={track.track.album.images[2].url} alt="album image" width={40} height={40} className="rounded-[0.8rem] mr-[1.2rem]"/>
             <div className="flex flex-col justify-center mr-[2rem]">
                 <div className="mb-[0.4rem] text-[1.6rem]">{track.track.name}</div>
                 <div className="text-[1.2rem] text-black/70 dark:text-white/70">{track.track.artists[0].name}</div>
+            </div>
             </div>
         </motion.a>
 
