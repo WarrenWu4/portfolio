82007677fde05b68641d94605c25f73ba1b6bdd8
WarrenWu4 <warrenweiwu04@gmail.com>
2024-01-10 20:18:54 -0600

    cleaned blog and error page

--- src/App.tsx
+++ src/App.tsx
@@ -62,8 +62,8 @@ export default function App() {
 function Error() {
   return (
     <>
-        <div className="mt-[12.8rem] mb-[3.2rem] text-[black] font-default font-bold text-[3.6rem] dark:text-dark-100">Error 404: Page Not Found</div>
-        <div className= "text-neutral-380 font-default font-bold text-[2.4rem] dark:text-dark-380">make sure the url is correct</div>
+        <div className="mt-32 font-bold text-4xl dark:text-dark-100">Error 404: Page Not Found</div>
+        <div className= "text-neutral-380 font-bold text-2xl dark:text-dark-380">make sure the url is correct</div>
     </>
   )
 }
--- src/pages/BlogPage.tsx
+++ src/pages/BlogPage.tsx
@@ -1,9 +1,9 @@
-import { motion } from "framer-motion";
 import { ReactMarkdown } from "react-markdown/lib/react-markdown";
 import data from "../data/blog.json";
 import { FiArrowRightCircle } from "react-icons/fi";
 import { Link, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
+import AnimatedLayout from "../layouts/AnimatedLayout";
 
 interface BlogData {
     title: string;
@@ -14,20 +14,14 @@ interface BlogData {
 
 export default function BlogPage() {
 
-    const variants = {
-        visible: {opacity: 1, transform: `translateY(0)`},
-        hidden: {opacity:0, transform: `translateY(2rem)`}
-    }
-    const timer = 1
-
-    
-
     return (
-        <div className="max-w-[768px] w-screen min-h-screen flex flex-col items-center relative px-[1.6rem] small:px-[3.2rem] med:px-[6.4rem]">
+        <>
 
-            <motion.div initial="hidden" whileInView="visible" viewport={{once: true}} transition={{duration:timer, ease:"easeInOut"}} variants={variants} className="mt-[4.8rem] w-full text-center huh:text-left font-bold text-[4rem]">Blog Posts</motion.div>
+            <AnimatedLayout className="w-full text-center huh:text-left font-bold text-[40px]">
+                Blog Posts
+            </AnimatedLayout>
 
-            <div className="w-full flex flex-wrap gap-[3.2rem] mt-[4.8rem]">
+            <div className="w-full flex flex-wrap gap-8">
             {
                 data["blogs"].map((metadata, index) => {
                     return <BlogItem key={index} title={metadata["title"]} article={metadata["article"]} dateCreated={metadata["date-created"]} desc={metadata["desc"]} ></BlogItem>
@@ -35,37 +29,29 @@ export default function BlogPage() {
             }
             </div>
 
-        </div>
+        </>
     )
 }
 
 const BlogItem = (data: BlogData) => {
     
-    const variants = {
-        visible: {opacity: 1, transform: `translateY(0)`},
-        hidden: {opacity:0, transform: `translateY(2rem)`}
-    }
-    const transition = {
-        duration: 1,
-        ease: "easeInOut"
-    }
-    
     return (
-        <motion.div initial="hidden" whileInView="visible" viewport={{once:true}} transition={transition} variants={variants} className="w-full rounded-[1.2rem] shadow-elevate dark:shadow-elevate-dark p-[1.6rem] group cursor-pointer">
+        <AnimatedLayout className="w-full rounded-xl shadow-elevate dark:shadow-elevate-dark p-4 group cursor-pointer">
             <Link to={`.${data.article}`}>
-                <div className="font-bold text-[2rem] mb-[0.4rem]">{data.title}</div>
-                <div className="w-full overflow-hidden text-[1.6rem] break-word line-clamp-4">{data.desc}</div>
+                <div className="font-bold text-[20px] mb-1">{data.title}</div>
 
-                <div className="w-full flex justify-between mt-[0.8rem]">
-                    <div className="text-black/70 dark:text-white/70 text-[1.6rem] font-bold">
+                <div className="w-full overflow-hidden text-[16px] break-word line-clamp-4">{data.desc}</div>
+
+                <div className="w-full flex justify-between mt-2">
+                    <div className="text-black/70 dark:text-white/70 text-[16px] font-bold">
                         {data.dateCreated}
                     </div>
-                    <div className="text-black/70 dark:text-white/70 flex font-bold text-[1.6rem] [&>svg]:ml-[0.4rem] [&>svg]:text-[2rem] group-hover:text-black dark:group-hover:text-white group-hover:translate-x-[0.4rem] transition-all duration-[0.4s]">
+                    <div className="text-black/70 dark:text-white/70 flex font-bold text-[16px] [&>svg]:ml-1 [&>svg]:text-[20px] group-hover:text-black dark:group-hover:text-white group-hover:translate-x-1 transition-all duration-[0.4s]">
                         Read More <FiArrowRightCircle className="translate-y-[0.16rem]" />
                     </div>
                 </div>
             </Link>
-        </motion.div>
+        </AnimatedLayout>
     )
 }
 
@@ -92,22 +78,14 @@ export const BlogArticle = () => {
 
     }, [])
 
-    const variants = {
-        visible: {opacity: 1, transform: `translateY(0)`},
-        hidden: {opacity:0, transform: `translateY(2rem)`}
-    }
-    const transition = {
-        duration: 1,
-        ease: "easeInOut"
-    }
 
     return (
-        <motion.div initial="hidden" whileInView={"visible"} viewport={{once: true}} transition={transition} variants={variants} className="max-w-[768px] w-screen min-h-screen flex flex-col items-center relative px-[1.6rem] small:px-[3.2rem] med:px-[6.4rem]">
+        <AnimatedLayout>
 
-            <div className="mt-[4.8rem] w-full flex flex-col" id="blog">
+            <div id="blog">
                 <ReactMarkdown children={mdInfo} />
             </div>
 
-        </motion.div>
+        </AnimatedLayout>
     )
 }
