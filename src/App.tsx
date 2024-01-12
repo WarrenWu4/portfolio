94cb6d8265c9248cf81ae51370dfeffa5f191755
WarrenWu4 <warrenweiwu04@gmail.com>
2024-01-12 13:52:45 -0600

    cleaned up icons on proj page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,6 @@
 import { Routes, Route, useLocation } from 'react-router-dom';
 import { useEffect } from 'react';
 
-import { FaPython, FaJava, FaReact, FaHtml5, FaCss3Alt, FaNodeJs, FaDocker } from "react-icons/fa";
-import { SiJavascript, SiCplusplus, SiFirebase, SiTailwindcss, SiPandas, SiTensorflow, SiNumpy, SiNextdotjs, SiDotnet, SiElectron, SiFlask, SiExpo, SiMysql, SiTypescript, SiMongodb, SiSvelte} from "react-icons/si";
-import { CgFigma } from "react-icons/cg";
-import { TbBrandThreejs } from "react-icons/tb";
-import { DiSqllite } from "react-icons/di";
-
 import HomePage from "./pages/HomePage";
 import { ProjPage, ProjInfo } from "./pages/ProjPage";
 import BlogPage, { BlogArticle } from './pages/BlogPage';
@@ -54,57 +48,4 @@ export default function App() {
 
     </MainLayout>
   )
-}
-
-// maps technologies as a string to actual icons
-export function IconGen(props:{icons:string[]|undefined}) {
-
-    if (props.icons === undefined) {
-        return [];
-    }
-
-    const iconDict:{[name:string]:JSX.Element} = {
-        "html": <FaHtml5/>,
-        "css": <FaCss3Alt />,
-        "javascript": <SiJavascript />,
-        "typescript": <SiTypescript/>,
-        "nextjs": <SiNextdotjs />,
-        "react": <FaReact />,
-        "nodejs": <FaNodeJs />,
-        "tailwindcss": <SiTailwindcss />,
-        "threejs": <TbBrandThreejs />,
-        "flask": <SiFlask />,
-        "svelte": <SiSvelte />,
-        "java": <FaJava />,
-        "c++": <SiCplusplus />,
-        "python": <FaPython/>,
-        "numpy": <SiNumpy />,
-        "pandas": <SiPandas />,
-        "tensorflow": <SiTensorflow />,
-        "firebase": <SiFirebase />,
-        "mongodb": <SiMongodb />,
-        "mysql": <SiMysql />,
-        "sqllite": <DiSqllite />,
-        "figma": <CgFigma />,
-        "expo": <SiExpo />,
-        ".net": <SiDotnet />,
-        "electronjs": <SiElectron />,
-        "docker": <FaDocker />,
-    }
-    
-    const iconStack:any[] = []
-
-    // if user request all icons
-    if (props.icons[0] === "all") {
-        Object.keys(iconDict).map((key) => {
-            iconStack.push(iconDict[key]);
-        });
-        return iconStack;
-    }
-
-    props.icons.map((name) => {
-        iconStack.push(iconDict[name]);
-    })
-
-    return iconStack;
 }
--- src/pages/ProjPage.tsx
+++ src/pages/ProjPage.tsx
@@ -1,10 +1,8 @@
-import { FiArrowRightCircle } from "react-icons/fi";
-import { BsGlobe, BsGithub } from "react-icons/bs";
 import { Link, useParams } from "react-router-dom";
 import { ReactMarkdown } from "react-markdown/lib/react-markdown";
 import { useEffect, useState } from "react";
+import { Icon } from "@iconify/react";
 
-import {IconGen} from "../App";
 import proj from "../data/proj.json";
 
 import AnimatedLayout from "../layouts/AnimatedLayout";
@@ -79,26 +77,28 @@ function ProjCard(data: ProjectData) {
 
     const githubComp = (data.github !== undefined) ? 
     <a target="_blank" href={data.github}>
-        <BsGithub className="text-black/80 hover:text-black dark:text-white/80 dark:hover:text-white transition-all duration-[0.2s] hover:scale-[120%]"/>
+        <Icon icon={"mdi:github"} className="text-black/80 hover:text-black dark:text-white/80 dark:hover:text-white transition-all duration-[0.2s] hover:scale-[120%]"/>
     </a> 
     :
     ""
 
     const weblinkComp = (data.link !== undefined) ? 
     <a target="_blank" href={data.link}>
-        <BsGlobe className="text-black/80 hover:text-black dark:text-white/80 dark:hover:text-white transition-all duration-[0.2s] hover:scale-[120%]" />
+        <Icon icon={"mdi:web"} className="text-black/80 hover:text-black dark:text-white/80 dark:hover:text-white transition-all duration-[0.2s] hover:scale-[120%]" />
     </a>
     :
     ""
-
-    const techIcons = IconGen({icons:data.techstack})
 
     return (
         <AnimatedLayout className="w-[300px] aspect-square shadow-elevate dark:shadow-elevate-dark rounded-xl flex flex-col relative overflow-hidden">
 
             <div className="border-b-2 border-solid border-[black] box-border w-full h-[55%] bg-neutral-360 bg-no-repeat bg-top bg-cover relative group" style={{backgroundImage: `url("${data.thumbnail}")`}}>
                 <div className="absolute top-0 left-0 w-full h-full bg-[black]/[0.8] font-bold font-default text-[white] text-[24px] flex justify-center items-center flex-wrap opacity-0 transition-[opacity] duration-[0.6s] ease-in-out group-hover:opacity-100 [&>*]:m-[0.8rem]">
-                    {techIcons}
+                    {
+                        data.techstack.map((icon:string, index:number) => {
+                            return <Icon key={index} icon={`simple-icons:${icon}`}/>
+                        })
+                    }
                 </div>
             </div>
 
@@ -111,8 +111,8 @@ function ProjCard(data: ProjectData) {
             </div>
 
             <Link to={"."+data.article}>
-            <div className="text-black/70 dark:text-white/70 absolute flex bottom-3 right-4 font-default font-bold text-[16px] cursor-pointer [&>svg]:ml-1 [&>svg]:text-[20px] hover:text-black dark:hover:text-white hover:translate-x-1 transition-all duration-[0.4s]">
-                    Learn More <FiArrowRightCircle className="translate-y-[0.16rem]" />
+            <div className="text-black/70 dark:text-white/70 absolute flex items-center bottom-3 right-4 font-default font-bold text-[16px] cursor-pointer [&>svg]:ml-1 [&>svg]:text-[20px] hover:text-black dark:hover:text-white hover:translate-x-1 transition-all duration-[0.4s]">
+                    Learn More <Icon icon={"mdi:arrow-right-thin-circle-outline"} />
             </div>
             </Link>
 
